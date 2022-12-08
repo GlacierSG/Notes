3db41e28be062d6b5f51341d0e2856c12b48ea31
GlacierSG <IamGlacierSG@gmail.com>
2022-12-08 14:35:18 +0000

    Added DES test

--- algorithm/cipher/symmetric/block/mode/test_modes.py
+++ algorithm/cipher/symmetric/block/mode/test_modes.py
@@ -3,9 +3,6 @@ import os
     
 
 ### Test Modes ###
-key = os.urandom(16)
-iv = os.urandom(16)
-nonce = os.urandom(8)
 
 
 from Crypto.Cipher import DES
@@ -18,39 +15,43 @@ from cfb import CFB
 from ofb import OFB
 from ctr import CTR
 
+ENC = [[AES, 16], [DES, 8]]
 
-Enc = AES
+for Enc, key_len in ENC:
+    key = os.urandom(key_len)
+    iv = os.urandom(key_len)
+    nonce = os.urandom(key_len//2)
 
-block = Enc.new(key, AES.MODE_ECB)
+    block = Enc.new(key, AES.MODE_ECB)
 
-tests = []
+    tests = []
 
-tests.append([
-    lambda : Enc.new(key, AES.MODE_ECB), ECB(block, key), True
-])
-tests.append([
-    lambda : Enc.new(key, AES.MODE_CBC, iv=iv), CBC(block, key, iv=iv), True
-])
-tests.append([
-    lambda : Enc.new(key, AES.MODE_CFB, iv=iv), CFB(block, key, iv=iv), False
-])
-tests.append([
-    lambda : Enc.new(key, AES.MODE_OFB, iv=iv), OFB(block, key, iv=iv), False
-])
-tests.append([
-    lambda : Enc.new(key, AES.MODE_CTR, nonce=nonce), CTR(block, key, nonce=nonce), False
-])
+    tests.append([
+        lambda : Enc.new(key, AES.MODE_ECB), ECB(block, key), True
+    ])
+    tests.append([
+        lambda : Enc.new(key, AES.MODE_CBC, iv=iv), CBC(block, key, iv=iv), True
+    ])
+    tests.append([
+        lambda : Enc.new(key, AES.MODE_CFB, iv=iv), CFB(block, key, iv=iv), False
+    ])
+    tests.append([
+        lambda : Enc.new(key, AES.MODE_OFB, iv=iv), OFB(block, key, iv=iv), False
+    ])
+    tests.append([
+        lambda : Enc.new(key, AES.MODE_CTR, nonce=nonce), CTR(block, key, nonce=nonce), False
+    ])
 
-for correct, test, padding in tests:
-    for i in range(1000):
-        msg = b'a'*i
-        if padding: msg = pad(msg, test.block_size)
-        encc = correct().encrypt(msg)
-        enct = test.encrypt(msg)
-        assert encc==enct, f"{test = }, ({encc = }) != ({enct = })"
-        decrc = correct().decrypt(encc)
-        decrt = test.decrypt(enct)
-        assert decrc == decrt
+    for correct, test, padding in tests:
+        for i in range(1000):
+            msg = b'a'*i
+            if padding: msg = pad(msg, test.block_size)
+            encc = correct().encrypt(msg)
+            enct = test.encrypt(msg)
+            assert encc==enct, f"{test = }, ({encc = }) != ({enct = })"
+            decrc = correct().decrypt(encc)
+            decrt = test.decrypt(enct)
+            assert decrc == decrt
 
 
 print("All tests have finished correctly")
