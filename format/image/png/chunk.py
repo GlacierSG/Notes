5bed1bd77ba104167bf93f0fd46721ba805e5392
GlacierSG <IamGlacierSG@gmail.com>
2022-12-23 15:43:19 +0000

    More compact code

--- format/image/png/chunk.py
+++ format/image/png/chunk.py
@@ -3,21 +3,21 @@ import logging
 PNG_HEADER = b'\x89PNG\r\n\x1a\n'
 
 class Chunk:
-	def __init__(self, file: bytes, i: int):
-		size = int.from_bytes(file[i:][:4],"big")
-		i += 4
-		self.type = file[i:][:4].decode()
-		i += 4
-		self.data = file[i:][:size]
-		i += size
-		self.crc = file[i:][:4]
-
-	def size(self):
-		return len(self.type) + len(self.data) + len(self.crc) + 4
+	def __init__(self, data: bytes, i: int):
+		self.size = int.from_bytes(data[i:(i:=i+4)],"big")
+		self.type = data[i:(i:=i+4)].decode()
+		self.data = data[i:(i:=i+self.size)]
+		self.crc = data[i:(i:=i+4)]
+	
+	def total_size(self):
+		return len(self.type) + len(self.data) \
+			 + len(self.crc) + 4
+	def calc_crc(self):
+		return zlib.crc32(self.type.encode() + self.data)
 	def check_crc(self):
-		return zlib.crc32(self.type.encode() + self.data).to_bytes(4,'big') == self.crc
+		return self.calc_crc() == int.from_bytes(self.crc,'big')
 	def __str__(self):
-		return f'Chunk(type: {self.type}, size: {self.size()})'
+		return f'Chunk(type: {self.type}, size: {self.size})'
 
 def get_chunks(file):
 	header = file[:len(PNG_HEADER)]
@@ -25,15 +25,15 @@ def get_chunks(file):
 		logging.warning(f'Incorrect png header, {PNG_HEADER} != {header}')
 	i = len(PNG_HEADER)
 	chunks = [Chunk(file, i)]
-	i += chunks[0].size()
+	i += chunks[0].total_size()
 	while True:
 		if i >= len(file): 
-			logging.warning('Unexpected end of file')
+			logging.error('Unexpected end of file')
 			break
 		chunks.append(Chunk(file, i))
 		if not chunks[-1].check_crc(): 
 			logging.warning(f'Wrong crc in chunk {chunks[-1]}, chunk nr: {len(chunks)}')
-		i += chunks[-1].size()
+		i += chunks[-1].total_size()
 		if chunks[-1].type == 'IEND': break
 	return chunks
 
--- format/image/png/png.py
+++ format/image/png/png.py
@@ -1,34 +1,20 @@
 from chunk import get_chunks
 import logging
 
-def ihdr(data, full=False):
-	i = 0
-	width = int.from_bytes(data[i:][:4],"big")
-	i += 4
-	height = int.from_bytes(data[i:][:4],"big")
-	i += 4
-	bit_depth = data[i] # 1,2,4,8,16
-	i += 1
-	color_type = data[i] # 0,2,3,4,6
-	i += 1
-	compression_method = data[i] # 0
-	i += 1
-	filter_method = data[i] # 0
-	i += 1
-	interlace_method = data[i] # 0, 1
-	
-	if full: 
-		return width, height, \
-			bit_depth, color_type, \
-			compression_method, \
-			filter_method, \
-			interlace_method
-	else:	
-		return width, height
+class IHDR:
+	def __init__(self, data):
+		i = 0
+		self.width = int.from_bytes(data[i:(i:=i+4)],"big")
+		self.height = int.from_bytes(data[i:(i:=i+4)],"big")
+		self.bit_depth = data[i:(i:=i+1)][0] # 1,2,4,8,16
+		self.color_type = data[i:(i:=i+1)][0] # 0,2,3,4,6
+		self.compression_method = data[i:(i:=i+1)][0] # 0
+		self.filter_method = data[i:(i:=i+1)][0] # 0
+		self.interlace_method = data[i:(i:=i+1)] # 0, 1
 
 if __name__ == '__main__':
 	normal = open('normal.png', 'rb').read()
 	chunks = get_chunks(normal)
 	print(chunks)
-	width,height = ihdr(chunks[0].data)
-	print(width,height)
+	ihdr = IHDR(chunks[0].data)
+	print(ihdr.width,ihdr.height)
